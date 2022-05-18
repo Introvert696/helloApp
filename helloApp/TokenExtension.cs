1b5719e3e6d27e4ad6e1fbcf2f6cbd3ead0c66ba
Introvert696 <introvert696@yandex.ru>
2022-05-18 16:17:32 +0400

    my learn files

---
+++ helloApp/TokenExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace helloApp
+{
+    public static class TokenExtension
+    {
+        public static IApplicationBuilder UseToken(this IApplicationBuilder bulder,string pattern)
+        {
+            return bulder.UseMiddleware<TokenMiddleware>(pattern);
+        }
+
+    }
+}
